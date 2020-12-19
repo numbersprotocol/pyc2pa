264f4ef2712e16079826e551c91b79a48a5a5b5d
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2020-12-19 22:30:30 +0800

    Fix hashlink unittest.
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- tests/test_hashlink.py
+++ tests/test_hashlink.py
@@ -7,7 +7,7 @@ class TestEncodeHashlink(unittest.TestCase):
     def test_hashlink(self):
         testing_data = b'Hello World!'
         mh = encode_hashlink(testing_data)
-        self.assertTrue(mh, b'zQmWvQxTqbG2Z9HPJgG57jjwR154cKhbtJenbyYTWkjgF3e')
+        self.assertEqual(mh, b'zQmWvQxTqbG2Z9HPJgG57jjwR154cKhbtJenbyYTWkjgF3e')
 
 
 if __name__ == '__main__':
