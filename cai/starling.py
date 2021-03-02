c3defd2793749fa2b7aa3b24ecbc8bb4e5c2d53b
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2021-03-02 19:03:39 +0800

    Make Starling to be an importable module.
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- cai/starling.py
+++ cai/starling.py
@@ -36,6 +36,153 @@ from cryptography.hazmat.primitives.serialization import pkcs12
 '''Starling CLI tool to generate CAI metadata.
 '''
 
+class Starling(object):
+    def __init__(self,
+                 media_bytes,
+                 media_name,
+                 raw_assertions,
+                 store_label,
+                 recorder,
+                 private_key='',
+                 signature_standard='cms'):
+        '''
+        raw_bytes: media content in bytes.
+
+        raw_assertions: dict of assertion JSON objects, and keys are labels.
+            refer to create_assertions for the details.
+
+        store_label: Example: cb.starling_1
+
+        recorder: The recorder field in Claim.
+
+        parent_claim: The parent_claim field in Claim. Example: self#jumbf=cai/cb.adobe/cai.claim
+
+        private_key: private key bytes.
+
+        signature_standard: CMS or CADES-B.
+        '''
+        self.raw_bytes = media_bytes
+        self.media_name = media_name
+        self.assertions = self.create_assertions(raw_assertions)
+        self.store_label = store_label
+        self.recorder = recorder
+        self.private_key = private_key
+        self.signature_standard = signature_standard
+        # get the label of the last Claim
+        self.parent_claim = get_xmp_tag(self.raw_bytes)
+        # get App11 marker segment headers
+        self.app11_headers = get_app11_marker_segment_headers(self.raw_bytes)
+        self.has_app11_headers = True if len(self.app11_headers) > 0 else False
+
+    def create_assertions(self, raw_assertions):
+        '''
+        raw_assertions: dict of assertion JSON objects, and keys are labels.
+            Example:
+                [
+                    {
+                        'cai.rights': {
+                            'type': 'json',
+                            'data_bytes': <data-bytes>
+                        }
+                    },
+                    ...
+                ]
+        return: list of Assertion SuperBoxes
+        '''
+        assertions = []
+        for k, v in raw_assertions.items():
+            label = k
+            data_bytes = v['data_bytes']
+            assertion_type = v['type']
+
+            if assertion_type == '.json':
+                assertions.append(create_json_superbox(content=data_bytes, label=label))
+            elif assertion_type == '.jpg':
+                assertions.append(create_codestream_superbox(content=data_bytes, label=label))
+            else:
+                raise Exception(
+                    'Unknown assertion type {0} from {1}'.format(assertion_type, label))
+        return assertions
+
+    def signel_claim_injection(self):
+        # create a new Store
+        cai_store = CaiStore(label=self.store_label,
+                             assertions=self.assertions,
+                             recorder=self.recorder,
+                             parent_claim=self.parent_claim,
+                             key=self.private_key,
+                             sig=self.signature_standard)
+
+        # create a new Claim Block Box
+        cai_claim_block = CaiClaimBlock()
+        cai_claim_block.content_boxes.append(cai_store)
+        cai_segment = App11Box()
+        cai_segment.payload = cai_claim_block.convert_bytes()
+
+        # save CAI-injected media
+        data_bytes = self.raw_bytes[0:2] + cai_segment.convert_bytes() + self.raw_bytes[2:]
+        cai_data_bytes = insert_xmp_key(data_bytes, store_label=self.store_label)
+        return cai_data_bytes
+
+    def multiple_claims_injection(self):
+        # generate acquisition assertion
+        acquisition_assertion = {
+            'dc:format': 'image/jpeg',
+            'dc:title': self.media_name,
+            'dcterms:provenance': self.parent_claim,
+            'stRef:DocumentID': 'xmp:fakeid:39afb1d3-7f8c-44e6-b771-85e0d9adb377',
+            'stRef:InstanceID': 'xmp:fakeid:10c04858-d3fd-4e2c-8947-7f1e29d62fbe',
+            'thumbnail': 'self#jumbf=cai/{}/cai.assertions/cai.acquisition.thumbnail.jpeg'.format(self.store_label)
+        }
+        self.assertions.append(create_json_superbox(
+            content=json_to_bytes(acquisition_assertion),
+            label='cai.acquisition_1'))
+
+        # create a new Store
+        cai_store = CaiStore(label=self.store_label,
+                             assertions=self.assertions,
+                             recorder=self.recorder,
+                             parent_claim=self.parent_claim,
+                             key=self.private_key,
+                             sig=self.signature_standard)
+
+        # get last segment header information
+        header_number = len(self.app11_headers)
+        last_en = self.app11_headers[header_number]['en']
+        last_lbox = self.app11_headers[header_number]['lbox']
+
+        # re-construct Claim Block payload
+        claim_block_payload = bytearray()
+        for i in range(1, header_number + 1):
+            payload_start = self.app11_headers[i]['offset'] + 20
+            payload_end = payload_start + (self.app11_headers[i]['le'] - 18)
+            payload = self.raw_bytes[payload_start : payload_end]
+            claim_block_payload += payload
+        store_bytes = cai_store.convert_bytes()
+
+        # append new Store bytes
+        updated_claim_block_payload = claim_block_payload + store_bytes
+        updated_lbox = last_lbox + len(store_bytes)
+        updated_claim_block_bytes = updated_lbox.to_bytes(4, byteorder='big') + b'jumb' + updated_claim_block_payload
+        updated_app11_segment = App11Box(en=last_en)
+        updated_app11_segment.payload = updated_claim_block_bytes
+
+        update_range_s = self.app11_headers[1]['offset']
+        update_range_e = self.app11_headers[1]['offset'] + last_lbox + 16
+
+        # save CAI-injected media
+        data_bytes = self.raw_bytes[:update_range_s] + updated_app11_segment.convert_bytes() + self.raw_bytes[update_range_e:]
+        cai_data_bytes = insert_xmp_key(data_bytes, store_label=self.store_label)
+        return cai_data_bytes
+
+    def cai_injection(self):
+        if self.has_app11_headers:
+            cai_data_bytes = self.multiple_claims_injection()
+        else:
+            cai_data_bytes = self.signel_claim_injection()
+        return cai_data_bytes
+
+
 def parse_args():
     ap = argparse.ArgumentParser()
     ap.add_argument(
@@ -61,10 +208,6 @@ def parse_args():
         '-s', '--sig',
         default='cms',
         help='cms or endesive')
-    ap.add_argument(
-        '-o', '--output',
-        default='',
-        help='Save CAI metadata to the filepath.')
     ap.add_argument(
         '-i', '--inject',
         default='',
@@ -98,26 +241,17 @@ def main():
     with open(args.inject, 'rb') as f:
         raw_bytes = f.read()
 
-    # get the label of the last Claim
-    parent_claim = get_xmp_tag(raw_bytes)
-
-    # get App11 marker segment headers
-    app11_headers = get_app11_marker_segment_headers(raw_bytes)
-    has_app11_headers = True if len(app11_headers) > 0 else False
-
     # create CAI metadata
-    assertions = []
+    raw_assertions = {}
     for filepath, label in zip(assertion_filepaths, assertion_labels):
         with open(filepath, 'rb') as f:
             data_bytes = f.read().strip()
         fileext = os.path.splitext(filepath)[1]
-        if fileext == '.json':
-            assertions.append(create_json_superbox(content=data_bytes, label=label))
-        elif fileext == '.jpg':
-            assertions.append(create_codestream_superbox(content=data_bytes, label=label))
-        else:
-            raise Exception(
-                'Unknown assertion type {0} from {1}'.format(fileext, filepath))
+
+        raw_assertions[label] = {
+            'type': fileext,
+            'data_bytes': data_bytes
+        }
 
     # private key for signature
     if key_filepath != '':
@@ -134,88 +268,22 @@ def main():
     else:
         key = []
 
-    if has_app11_headers:
-        # generate acquisition assertion
-        acquisition_assertion = {
-            'dc:format': 'image/jpeg',
-            'dc:title': os.path.basename(args.inject),
-            'dcterms:provenance': parent_claim,
-            'stRef:DocumentID': 'xmp:fakeid:39afb1d3-7f8c-44e6-b771-85e0d9adb377',
-            'stRef:InstanceID': 'xmp:fakeid:10c04858-d3fd-4e2c-8947-7f1e29d62fbe',
-            'thumbnail': 'self#jumbf=cai/{}/cai.assertions/cai.acquisition.thumbnail.jpeg'.format(store_label)
-        }
-        assertions.append(create_json_superbox(
-            content=json_to_bytes(acquisition_assertion),
-            label='cai.acquisition_1'))
+    # create CAI-injected media
+    starling = Starling(raw_bytes,
+                        os.path.basename(args.inject),
+                        raw_assertions,
+                        store_label,
+                        recorder,
+                        key,
+                        type_sig)
+    starling_cai_bytes = starling.cai_injection()
 
-        # create a new Store
-        cai_store = CaiStore(label=store_label,
-                             assertions=assertions,
-                             recorder=recorder,
-                             parent_claim=parent_claim,
-                             key=key,
-                             sig=type_sig)
-
-        # get last segment header information
-        header_number = len(app11_headers)
-        last_en = app11_headers[header_number]['en']
-        last_lbox = app11_headers[header_number]['lbox']
-
-        # re-construct Claim Block payload
-        claim_block_payload = bytearray()
-        for i in range(1, header_number + 1):
-            payload_start = app11_headers[i]['offset'] + 20
-            payload_end = payload_start + (app11_headers[i]['le'] - 18)
-            payload = raw_bytes[payload_start : payload_end]
-            claim_block_payload += payload
-        store_bytes = cai_store.convert_bytes()
-
-        # append new Store bytes
-        updated_claim_block_payload = claim_block_payload + store_bytes
-        updated_lbox = last_lbox + len(store_bytes)
-        updated_claim_block_bytes = updated_lbox.to_bytes(4, byteorder='big') + b'jumb' + updated_claim_block_payload
-        updated_app11_segment = App11Box(en=last_en)
-        updated_app11_segment.payload = updated_claim_block_bytes
-
-        update_range_s = app11_headers[1]['offset']
-        update_range_e = app11_headers[1]['offset'] + last_lbox + 16
-
-        # save CAI-injected media
-        if len(args.inject) > 0:
-            fname, fext = os.path.splitext(args.inject)
-            fpath = fname + '-cai' + fext
-            with open(fpath, 'wb') as f:
-                data_bytes = raw_bytes[:update_range_s] + updated_app11_segment.convert_bytes() + raw_bytes[update_range_e:]
-                cai_data_bytes = insert_xmp_key(data_bytes, store_label=store_label)
-                f.write(cai_data_bytes)
-    else:
-        # create a new Store
-        cai_store = CaiStore(label=store_label,
-                             assertions=assertions,
-                             recorder=recorder,
-                             parent_claim=parent_claim,
-                             key=key,
-                             sig=type_sig)
-
-        # create a new Claim Block Box
-        cai_claim_block = CaiClaimBlock()
-        cai_claim_block.content_boxes.append(cai_store)
-        cai_segment = App11Box()
-        cai_segment.payload = cai_claim_block.convert_bytes()
-
-        # save CAI-injected media
-        if len(args.inject) > 0:
-            fname, fext = os.path.splitext(args.inject)
-            fpath = fname + '-cai' + fext
-            with open(fpath, 'wb') as f:
-                data_bytes = raw_bytes[0:2] + cai_segment.convert_bytes() + raw_bytes[2:]
-                cai_data_bytes = insert_xmp_key(data_bytes, store_label=store_label)
-                f.write(cai_data_bytes)
-
-    # save CAI metadata
-    if len(args.output) > 0:
-        with open(args.output, 'w') as f:
-            f.write(cai_segment.convert_bytes().hex())
+    # save CAI-injected media
+    if len(args.inject) > 0:
+        fname, fext = os.path.splitext(args.inject)
+        fpath = fname + '-cai' + fext
+        with open(fpath, 'wb') as f:
+            f.write(starling_cai_bytes)
 
 
 if __name__ == "__main__":
